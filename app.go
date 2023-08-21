e012a271a6d8dddf03d53d170a1e30dbcef08bb1
Joseph Mureithi <ngatia.joseph@gmail.com>
2023-08-21 15:05:14 +0300

    implemented test and initialize method

---
+++ app.go
@@ -0,0 +1,32 @@
+// app.go
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) { 
+	connectionString :=
+	fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+
+var err error
+a.DB, err = sql.Open("postgres", connectionString)
+if err != nil {
+	log.Fatal(err)
+}
+
+a.Router = mux.NewRouter()  
+}
+
+func (a *App) Run(addr string) { }
