0c87e56239d26d05c50b3f78d23d837c3c0081dc
Joseph Mureithi <ngatia.joseph@gmail.com>
2023-08-22 20:53:40 +0300

    routes added

--- app.go
+++ app.go
@@ -7,6 +7,10 @@ import (
 	"fmt"
 	"log"
 
+	"net/http"
+    "strconv"
+    "encoding/json"
+
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -17,7 +21,7 @@ type App struct {
 }
 
 func (a *App) Initialize(user, password, dbname string) {
-	
+
     connectionString :=
         fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
   
@@ -31,4 +35,129 @@ func (a *App) Initialize(user, password, dbname string) {
     a.Router = mux.NewRouter()
 }
 
-func (a *App) Run(addr string) { }
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8010", a.Router))
+ }
+
+ func (a *App) getArt(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid art ID")
+		return
+	}
+
+	p := art{ID: id}
+	if err := p.getArt(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "art not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+
+func (a *App) getArts(w http.ResponseWriter, r *http.Request) {
+	count, _ := strconv.Atoi(r.FormValue("count"))
+	start, _ := strconv.Atoi(r.FormValue("start"))
+
+	if count > 10 || count < 1 {
+		count = 10
+	}
+	if start < 0 {
+		start = 0
+	}
+
+	arts, err := getArts(a.DB, start, count)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, arts)
+}
+
+func (a *App) createArt(w http.ResponseWriter, r *http.Request) {
+	var p art
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := p.createArt(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, p)
+}
+
+func (a *App) updateArt(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid art ID")
+		return
+	}
+
+	var p art
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
+		return
+	}
+	defer r.Body.Close()
+	p.ID = id
+
+	if err := p.updateArt(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) deleteArt(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid art ID")
+		return
+	}
+
+	p := art{ID: id}
+	if err := p.deleteArt(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/arts", a.getArts).Methods("GET")
+	a.Router.HandleFunc("/art", a.createArt).Methods("POST")
+	a.Router.HandleFunc("/art/{id:[0-9]+}", a.getArt).Methods("GET")
+	a.Router.HandleFunc("/art/{id:[0-9]+}", a.updateArt).Methods("PUT")
+	a.Router.HandleFunc("/art/{id:[0-9]+}", a.deleteArt).Methods("DELETE")
+}
